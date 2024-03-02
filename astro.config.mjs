4523f0894ec8578aa81c2c2f5785b2e3cf0d7df1
Nicolás González <nicolook.pro@gmail.com>
2024-03-02 16:53:47 -0300

    Remove Astro Icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],

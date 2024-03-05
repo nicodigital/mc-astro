7521c639b8dda85aa9f753b3cf4729ab6d9e7f28
Nicolás González <nicolook.pro@gmail.com>
2024-03-05 18:37:21 -0300

    Client Side Dynamic Clock

--- .netlify/functions-internal/ssr/chunks/_name__BnCZpbjN.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_Dk4i-AXj.mjs').then(n => n._);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/_name__Bu4UZ7a-.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/_name__7XkyCO5V.mjs').then(n => n._);
-
-export { page };

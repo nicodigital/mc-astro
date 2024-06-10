d6070be467e9b73b82cd1473df0637661b2afef4
Nicolás González <nicolook.pro@gmail.com>
2024-06-10 08:30:22 -0300

    Add Coming Soon screen

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,5 +1,4 @@
 function animations() {
-
   const observedElements = new Set();
 
   const animations = document.querySelectorAll(".animate");
@@ -10,24 +9,22 @@ function animations() {
     /* Observer */
     const triggerAnim = (entries) => {
       entries.forEach((entry) => {
-
         const delay = entry.target.dataset.delay || 0;
-
+        const speed = entry.target.dataset.speed+"s" || '1s';
         const once = entry.target.dataset.once ?? "false"; // false 
 
-        if (entry.isIntersecting) {
+        if (entry.isIntersecting || entry.target.classList.contains('force-anim')) {
           setTimeout(() => {
+            entry.target.style.animationDuration = speed;
             entry.target.classList.add('anim-on');
-            if (once) {
+            if (once === "true") {
               observedElements.add(entry.target);
             }
           }, delay);
         } else {
-
-          if ( once != 'true' ) {
-           entry.target.classList.remove('anim-on');
+          if (once !== 'true') {
+            entry.target.classList.remove('anim-on');
           }
-
         }
       });
     };
@@ -47,8 +44,17 @@ function animations() {
     animations.forEach(element => {
       observer.observe(element);
     });
-  }
 
+    // Trigger animations for elements with .force-anim class on page load
+    document.querySelectorAll('.force-anim').forEach(element => {
+      const delay = element.dataset.delay || 0;
+      const speed = element.dataset.speed+"s" || '1s';
+      setTimeout(() => {
+        element.style.animationDuration = speed;
+        element.classList.add('anim-on');
+      }, delay);
+    });
+  }
 }
 
 export default animations;
